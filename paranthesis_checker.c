212a7534a46bcecd0c68d03ad1851e3f26996f6c
nilam576 <71421769+nilam576@users.noreply.github.com>
2023-10-29 00:24:15 +0530

    Update paranthesis_checker.c

--- paranthesis_checker.c
+++ paranthesis_checker.c
@@ -1,55 +1,72 @@
-#include<stdio.h>
-#include<conio.h>
-#include<string.h>
-int top=-1;
-char stack[10];
+#include <stdio.h>
+#include <stdbool.h>
+#include <string.h>
 
-void push(char ch){
-if(top==9)
-	printf("Stack is full");
-else
-	stack[++top]=ch;
+#define MAX_SIZE 100
+
+// Improved stack implementation
+struct Stack {
+    int top;
+    char items[MAX_SIZE];
+};
+
+void initialize(struct Stack *s) {
+    s->top = -1;
 }
 
-char pop(){
-char c1;
-if(top==-1)
-	printf("stack is empty");
-else
-	c1=stack[top--];
-return c1;
+void push(struct Stack *s, char c) {
+    if (s->top >= MAX_SIZE - 1) {
+        printf("Stack is full\n");
+    } else {
+        s->items[++(s->top)] = c;
+    }
 }
 
-
-void main(){
-char exp[10],tp;
-int i,flag=1;
-clrscr();
-printf("Enter the expression: ");
-gets(exp);
-for(i=0;i<strlen(exp);i++){
-	if(exp[i]=='('||exp[i]=='{'||exp[i]=='['){
-		push(exp[i]);
-	}
-	if(exp[i]==')'||exp[i]=='}'||exp[i]==']'){
-		if(top==-1)
-			flag=0;
-		else
-			tp=pop();
-	}
-	if(exp[i]==')'&&(tp=='{'||tp=='['))
-		flag=0;
-	if(exp[i]=='}'&&(tp=='('||tp=='['))
-		flag=0;
-	if(exp[i]==']'&&(tp=='{'||tp=='('))
-		flag=0;
+char pop(struct Stack *s) {
+    if (s->top == -1) {
+        printf("Stack is empty\n");
+        return '\0';
+    } else {
+        return s->items[(s->top)--];
+    }
 }
-if(top>=0)
-	flag=0;
-if(flag==1)
-	printf("match");
-else
-	printf("Mismatch");
 
-getch();
+bool isMatchingPair(char opening, char closing) {
+    if (opening == '(' && closing == ')')
+        return true;
+    else if (opening == '{' && closing == '}')
+        return true;
+    else if (opening == '[' && closing == ']')
+        return true;
+    else
+        return false;
+}
+
+bool checkBalancedParentheses(char exp[]) {
+    struct Stack s;
+    initialize(&s);
+    int i;
+    for (i = 0; i < strlen(exp); i++) {
+        if (exp[i] == '(' || exp[i] == '{' || exp[i] == '[') {
+            push(&s, exp[i]);
+        } else if (exp[i] == ')' || exp[i] == '}' || exp[i] == ']') {
+            if (s.top == -1 || !isMatchingPair(pop(&s), exp[i])) {
+                return false;
+            }
+        }
+    }
+    return s.top == -1;
+}
+
+int main() {
+    char exp[MAX_SIZE];
+    printf("Enter the expression: ");
+    fgets(exp, MAX_SIZE, stdin);
+
+    if (checkBalancedParentheses(exp)) {
+        printf("Parentheses are matched\n");
+    } else {
+        printf("Parentheses are not matched\n");
+    }
+    return 0;
 }
